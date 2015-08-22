ab246a70a4e774ce18958031d0fdb24fe7329123
yanguango <yanguango@gmail.com>
2015-08-22 16:43:31 -0400

    improve WordSearch

--- WordSearch.java
+++ WordSearch.java
@@ -2,29 +2,27 @@ import java.util.Arrays;
 
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
-        boolean[][] used = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                for (boolean[] row : used) Arrays.fill(row, false);
-                if (search(board, i, j, used, word, 0)) return true;
+                if (dfs(board, i, j, word, 0)) return true;
                 else continue;
             }
         }
         return false;
     }
 
-    public boolean search(char[][] board, int i, int j,
-                          boolean[][] used, String word, int index) {
+    public boolean dfs(char[][] board, int i, int j,
+                       String word, int index) {
         if (index > word.length() - 1) return true;
-        if (i < 0 || i > board.length - 1) return false;
-        if (j < 0 || j > board[0].length - 1) return false;
-        if (used[i][j] || board[i][j] != word.charAt(index)) return false;
-        used[i][j] = true;
-        boolean result = (search(board, i + 1, j, used, word, index + 1) ||
-                          search(board, i - 1, j, used, word, index + 1) ||
-                          search(board, i, j + 1, used, word, index + 1) ||
-                          search(board, i, j - 1, used, word, index + 1));
-        used[i][j] = false;
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) return false;
+        if (board[i][j] != word.charAt(index)) return false;
+        char temp = board[i][j];
+        board[i][j] = '$';
+        boolean result = (dfs(board, i + 1, j, word, index + 1) ||
+                          dfs(board, i - 1, j, word, index + 1) ||
+                          dfs(board, i, j + 1, word, index + 1) ||
+                          dfs(board, i, j - 1, word, index + 1));
+        board[i][j] = temp;
         return result;
 
     }
