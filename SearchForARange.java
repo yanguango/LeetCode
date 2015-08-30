6976ad14a7f791dab0115d15277e55b250b5d186
yanguango <yanguango@gmail.com>
2015-08-30 16:03:32 -0400

    SearchForARange

--- SearchForARange.java
+++ SearchForARange.java
@@ -2,6 +2,41 @@ import java.util.*;
 
 public class SearchForARange {
     public int[] searchRange(int[] nums, int target) {
+        int lower = lowerBound(nums, target, 0, nums.length);
+        int upper = upperBound(nums, target, lower, nums.length);
+        if (lower > nums.length - 1 || nums[lower] != target) {
+            return new int[] { -1, -1};
+        } else {
+            return new int[] {lower, upper - 1};
+        }
+    }
+
+    public int lowerBound(int[] nums, int target, int start, int end) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    public int upperBound(int[] nums, int target, int start, int end) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    //Recursive
+    public int[] searchRangeRecur(int[] nums, int target) {
         int range_start = -1, range_end = -1;
         int[] res = new int[2];
         res[0] = searchRangeStart(nums, target, 0, nums.length - 1);
