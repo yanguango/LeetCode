a7c985ccce84c9f45ff85573a1f5e5a4c88ad880
yanguango <yanguango@gmail.com>
2015-08-31 15:06:04 -0400

    SearchInRotatedSortedArray2

---
+++ SearchInRotatedSortedArray2.java
@@ -0,0 +1,20 @@
+public class SearchInRotatedSortedArray2 {
+    public boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] < nums[end]) {
+                if (target > nums[mid] && target <= nums[end]) start = mid + 1;
+                else end = mid - 1;
+            } else if (nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) end = mid - 1;
+                else start = mid + 1;
+            } else {
+                end--;
+            }
+        }
+        return false;
+    }
+}
