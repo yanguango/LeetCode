ee45e1875148b55f991a54a09c4686d51ef53364
yanguango <yanguango@gmail.com>
2015-09-01 11:35:14 -0400

    SingleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
