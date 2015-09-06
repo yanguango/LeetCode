855b217750372bccf524f44b4bbfe53e709c8b74
yanguango <yanguango@gmail.com>
2015-09-05 23:50:37 -0400

    change UglyNumber

--- UglyNumber.java
+++ UglyNumber.java
@@ -2,12 +2,9 @@ public class UglyNumber {
     public boolean isUgly(int num) {
         if (num <= 0) return false;
         if (num == 1) return true;
-        if (num == 2 || num == 3 || num == 5) {
-            return true;
-        } else {
-            return (num % 2 == 0 ? isUgly(num / 2) : false) ||
-                   (num % 3 == 0 ? isUgly(num / 3) : false) ||
-                   (num % 5 == 0 ? isUgly(num / 5) : false);
-        }
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
     }
 }
