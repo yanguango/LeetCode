d770344f47a2ddd30c4e008fc155ecd20c47563a
yanguango <yanguango@gmail.com>
2015-09-07 10:55:45 -0400

    add comment

--- FactorialTrailingZeros.java
+++ FactorialTrailingZeros.java
@@ -1,6 +1,8 @@
 public class FactorialTrailingZeros {
     public int trailingZeroes(int n) {
         int count = 0;
+        // m maybe exceeds Integer.MAX_VALUE
+        // use long to hold m
         long m = 5;
         while (m <= n) {
             count += (n / m);
