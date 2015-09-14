d5db162a16d6f3979698d5a88fea3c41ea00ddae
yanguango <yanguango@gmail.com>
2015-09-14 11:35:16 -0400

    BinaryTreeLevelOrderTraversal2

---
+++ BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeLevelOrderTraversal2 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            res.add(list);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
