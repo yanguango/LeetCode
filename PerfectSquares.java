8da3a40a3947dc8b1f31b2d2667e5928a1e31b24
yanguango <yanguango@gmail.com>
2015-09-24 11:19:27 -0400

    PerfectSquares

---
+++ PerfectSquares.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; i + j * j <= n; j++) {
+                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
