eeea94bc7cd83b04daf34dda5dd55f35c94f2ad9
yanguango <yanguango@gmail.com>
2015-10-23 16:59:07 -0400

    BinaryTreeUpsideDown

--- BinaryTreeUpsideDown.java
+++ BinaryTreeUpsideDown.java
@@ -18,3 +18,25 @@ public class Solution {
         return newRoot;
     }
 }
+
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) return root;
+        TreeNode newRoot = root.left;
+        TreeNode left = root.right;
+        TreeNode right = root;
+        root.left = null;
+        root.right = null;
+        while (newRoot != null) {
+            TreeNode nextRoot = newRoot.left;
+            TreeNode newLeft = newRoot.right;
+            newRoot.right = right;
+            newRoot.left = left;
+            left = newLeft;
+            right = newRoot;
+            if (nextRoot == null) break;
+            newRoot = nextRoot;
+        }
+        return newRoot;
+    }
+}
